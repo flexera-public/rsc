79b4b3d8f587f3a0a73d6daa2303185c885e2a2e
Douglas Thrift <douglaswth@gmail.com>
2015-05-12 18:22:02 -0700

    Add RightLink 10 TSS actions.

---
+++ rl10/design/resources/tss.rb
@@ -0,0 +1,27 @@
+module Resources
+  class TSS
+    include Praxis::ResourceDefinition
+    
+    description 'Manipulate the TSS proxy'
+    media_type 'text/plain'
+    
+    routing do
+      prefix '/rll/tss'
+    end
+
+    action :get_hostname do
+      description 'Set the TSS hostname to proxy'
+      routing { get '/hostname' }
+      response :ok
+    end
+
+    action :put_hostname do
+      description 'Set the TSS hostname to proxy'
+      routing { put '/hostname' }
+      params do
+        attribute :hostname, String, required: true
+      end
+      response :ok
+    end
+  end
+end
