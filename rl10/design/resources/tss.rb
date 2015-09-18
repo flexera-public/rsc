d7ec4d787801042508756280aceccafe9fe6c8d2
Thorsten von Eicken <tve@rightscale.com>
2015-09-18 12:42:41 -0700

    update rl10 TSS calls using praxis DSL

--- rl10/design/resources/tss.rb
+++ rl10/design/resources/tss.rb
@@ -9,24 +9,50 @@ module Resources
       prefix '/rll/tss'
     end
 
-    action :put_control do
+    action :update do
       description 'Control the TSS monitoring'
       routing { put '/control' }
       params do
         attribute :tss_id, String, required: false
-        attribute :enable_monitoring, Attributor::Boolean, required: false
+        attribute :enable_monitoring, Attributor::String, required: false,
+		values:['false','true','none','util','extra','all']
       end
       response :ok
     end
 
-    action :get_hostname do
+    action :put_control do
+      description 'Control the TSS monitoring (deprecated)'
+      routing { put '/control' }
+      params do
+        attribute :tss_id, String, required: false
+        attribute :enable_monitoring, Attributor::String, required: false, example: 'all'
+      end
+      response :ok
+    end
+
+    action :show do
       description 'Get the TSS hostname to proxy'
       routing { get '/hostname' }
       response :ok
     end
 
-    action :put_hostname do
+    action :get_hostname do
+      description 'Get the TSS hostname to proxy (deprecated)'
+      routing { get '/hostname' }
+      response :ok
+    end
+
+    action :update do
       description 'Set the TSS hostname to proxy'
+      routing { put '/hostname' }
+      params do
+        attribute :hostname, String, required: true
+      end
+      response :ok
+    end
+
+    action :put_hostname do
+      description 'Set the TSS hostname to proxy (deprecated)'
       routing { put '/hostname' }
       params do
         attribute :hostname, String, required: true
