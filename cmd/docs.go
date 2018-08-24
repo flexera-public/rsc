ea0f40d9864d460cfbb214ecca21f7a5a63ece15
Marco Dominguez <marco@rightscale.com>
2018-08-24 11:12:48 -0700

    Support with_deleted parameter in instance_types show and index calls. v6.10.0

--- cmd/docs.go
+++ cmd/docs.go
@@ -1,3 +1,3 @@
 // Package cmd is a tiny package that contains common data types shared between the main rsc
 // command line parser and the API client command line tools.
-package cmd
+package cmd // import "gopkg.in/rightscale/rsc.v6/cmd"
